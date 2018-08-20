aab7c0ca4ce575772fe6829c4f5b1b34a4d815f0
Akmal Khadir <akmal.a.khadir@gmail.com>
2018-08-20 21:58:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 function dwarfRollCall(dwarves) {
   let numberedDwarves = []
-  for (let i=0; i < dwarves.length; i++){
-    numberedDwarves.push(`${i+1}. ${dwarves[i]} `) //1. name
+  for (let i=0; i < dwarves.length; i++) {
+    numberedDwarves.push(`${i+1}. ${dwarves[i]} `)
   }
   return numberedDwarves.join(``)
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
   let shoutPlaneteer = []
-  for (let i=0; i<planeteerCalls.length; i++){
-    shoutPlaneteer.push(`${planeteerCalls[i].toUpperCase}!`)
+  for (let i=0; i<planeteerCalls.length; i++) {
+    shoutPlaneteer.push(`${planeteerCalls[i].toUpperCase()}!`)
   }
   return shoutPlaneteer
 }
 
 function longPlaneteerCalls(words) {
-  let longCounter = 0                       // Counter to keep register long words
-  for (let i = 0; i < words.length; i++){   // Loop through the elements, check if a word is longer than 4 char, add 1 to the counter
-   if (words[i].length > 4){
-    longCounter++
+  let longCounter = 0                        // Counter to keep register long words
+  for (let i = 0; i < words.length; i++) {   // Loop through the elements, check if a word is longer than 4 char, add 1 to the counter
+    if (words[i].length > 4) {
+      longCounter++
     }
-  }
-    if (longCounter > 0){                   // End of loop, check if counter has registered any long words
+  } if (longCounter > 0) {                   // End of loop, check if counter has registered any long words
     return true
-    } else return false
+  } else return false
 }
 
 
 
 function findTheCheese (foods) {
-  function cheeseTest(x){                                                       // Testing (callback) function to be used by the findIndex method
+  function cheeseTest(x){                                           // Testing (callback) function to be used by the findIndex method
     return x === `cheddar` || x === `gouda` || x === `camembert`
   }
-    if (foods.findIndex(cheeseTest) < 0 ) {
+  let cheeseIndex = foods.findIndex(cheeseTest)                    // Will return the position of the cheese, -1 if no cheese found
+  if (cheeseIndex < 0 ) {
     return `no cheese!`
-  } if (foods.findIndex(cheeseTest) >= 0 )
-  return `${foods[foods.findIndex(cheeseTest)]}`
+  } if (cheeseIndex >= 0 )
+    return `${foods[cheeseIndex]}`
 }
