27ced6a2d27571a9ad4bbf542a75b9e79ba6b6ae
Akmal Khadir <akmal.a.khadir@gmail.com>
2018-08-20 21:56:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 function dwarfRollCall(dwarves) {
   let numberedDwarves = []
-  for (let i=0; i < dwarves.length; i++){
+  for (let i=0; i < dwarves.length; i++) {
     numberedDwarves.push(`${i+1}. ${dwarves[i]} `)
   }
   return numberedDwarves.join(``)
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
   let shoutPlaneteer = []
-  for (let i=0; i<planeteerCalls.length; i++){
+  for (let i=0; i<planeteerCalls.length; i++) {
     shoutPlaneteer.push(`${planeteerCalls[i].toUpperCase()}!`)
   }
   return shoutPlaneteer
 }
 
 function longPlaneteerCalls(words) {
-  let longCounter = 0                       // Counter to keep register long words
-  for (let i = 0; i < words.length; i++){   // Loop through the elements, check if a word is longer than 4 char, add 1 to the counter
-   if (words[i].length > 4){
-    longCounter++
+  let longCounter = 0                        // Counter to keep register long words
+  for (let i = 0; i < words.length; i++) {   // Loop through the elements, check if a word is longer than 4 char, add 1 to the counter
+    if (words[i].length > 4) {
+      longCounter++
     }
-  }
-    if (longCounter > 0){                   // End of loop, check if counter has registered any long words
+  } if (longCounter > 0) {                   // End of loop, check if counter has registered any long words
     return true
-    } else return false
+  } else return false
 }
 
 
